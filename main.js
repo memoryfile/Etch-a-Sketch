ecc171b4131a57049312be0468ff04e9612a2539
memoryfile-desktop <yuiotp999@protonmail.com>
2022-07-01 13:55:14 -0400

    Add height & width to grids

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const grids = document.querySelector("#grids");
 const cell = document.createElement("div");
 cell.innerText = "test";
 grids.append(cell);
+
+let divContainer = document.getElementById("grids");
