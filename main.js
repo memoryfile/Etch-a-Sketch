fdcca4584930c0c333d01737c68cf53a6017fe47
memoryfile-desktop <yuiotp999@protonmail.com>
2022-07-04 14:41:12 -0400

    Add gridd to makeGrids()

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const grids = document.querySelector("#grids");
 
+// const gridd = document.getElementById("head");
+// gridd.innerText("head2");
+
 const cell = document.createElement("div");
 cell.innerText = "test";
 grids.append(cell);
 
 let divContainer = document.getElementById("grids");
+
+function makeGrids() {
+  for (let i = 0; i < 16; i++) {
+    const gridd = document.getElementById(grids);
+    gridd.classList.add('head');
+    gridd.innerText = "test2";
+    console.log(gridd)
+    gridd.style.cssText = "color: pink; border:2px black";
+    grids.append(head);
+  }
+}
+
+makeGrids();
