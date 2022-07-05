d90805700ea5ba83a244b4e6176317b70a133bc2
memoryfile <cooperlewis@protonmail.com>
2022-07-05 15:17:00 -0400

    Add title and remove unnecessary JS/CSS

--- main.js
+++ main.js
@@ -2,11 +2,7 @@ function makeGrids() {
   for (let i = 0; i < 16 * 16; i++) {
     const grids = document.querySelector("#grids");
     const cells = document.createElement("cells");
-
-    // // cells.innerText = "grid";
     grids.append(cells);
-    const square = document.querySelector(".square");
-    grids.append(square);
   }
 }
 
