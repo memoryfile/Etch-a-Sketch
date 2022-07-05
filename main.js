093d145fa829d471cf531ad55e2af0595ed417e7
memoryfile <cooperlewis@protonmail.com>
2022-07-05 15:06:16 -0400

    Create square divs

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 function makeGrids() {
-  for (let i = 0; i < (16 * 16); i++) {
+  for (let i = 0; i < 16 * 16; i++) {
     const grids = document.querySelector("#grids");
-    const testClass = document.createElement("testClass");
-    testClass.innerText = "grid";
-    testClass.style.cssText = "color: blue; border:2px black";
-    grids.append(testClass);
+    const cells = document.createElement("cells");
+
+    // // cells.innerText = "grid";
+    grids.append(cells);
+    const square = document.querySelector(".square");
+    grids.append(square);
   }
 }
 
