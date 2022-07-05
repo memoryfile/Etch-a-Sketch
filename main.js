8196f451f2550eafebcf94965ee4c398f55c5338
memoryfile <cooperlewis@protonmail.com>
2022-07-05 16:59:59 -0400

    Change background color

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ function makeGrids() {
     const grids = document.querySelector("#grids");
     const cells = document.createElement("cells");
     grids.append(cells);
+
+    function hoverEffect() {
+      grids.addEventListener("mouseenter", enter);
+      grids.addEventListener("mouseleave", leave);
+    }
   }
 }
 
