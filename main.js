504c9214c0bac7e7950aae77e009c54505d4e27c
memoryfile <cooperlewis@protonmail.com>
2022-07-06 18:08:32 -0400

    Initial commit for button functionality

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const blackButton = document.querySelector("blackButton");
+
 function createCells() {
   const grids = document.querySelector("#grids");
-  grids.style.gridTemplateColumns = "repeat(16, 1fr)";
-  grids.style.gridTemplateRows = "repeat(16, 1fr)";
+  // grids.style.gridTemplateColumns = "repeat(16, 1fr)";
+  // grids.style.gridTemplateRows = "repeat(16, 1fr)";
 
   function makeGrids() {
     for (let i = 0; i < 256; i++) {
@@ -9,10 +11,29 @@ function createCells() {
       grids.append(cells);
 
       function hoverEffect() {
-        cells.addEventListener("mouseenter", () => cells.style.backgroundColor = "black");
-        cells.addEventListener("mouseleave", () => cells.style.backgroundColor = "white");
+        cells.addEventListener(
+          "mouseenter",
+          () => (cells.style.backgroundColor = "black")
+        );
+        cells.addEventListener(
+          "mouseleave",
+          () => (cells.style.backgroundColor = "white")
+        );
       }
       hoverEffect();
+
+      function cellsBecomeBlack() {
+        blackButton.addEventListener("click", () =>
+        cells.style.backgroundColor = "black");
+      }
+
+      function cellsBecomeWhite() {
+        cells.style.backgroundColor = "white";
+      }
+
+      function cellsBecomeRandom() {
+        // cells.style.backgroundColor = "black";
+      }
     }
   }
 
