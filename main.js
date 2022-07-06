dcf3e78788c77b308c331f4b634e77562475cee3
memoryfile <cooperlewis@protonmail.com>
2022-07-06 14:56:54 -0400

    Customize buttons and amount of cells

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-function makeGrids() {
-  for (let i = 0; i < 16 * 16; i++) {
-    const grids = document.querySelector("#grids");
-    const cells = document.createElement("cells");
-    grids.append(cells);
+function createCells() {
+  const grids = document.querySelector("#grids");
+  grids.style.gridTemplateColumns = "repeat(16, 1fr)";
+  grids.style.gridTemplateRows = "repeat(16, 1fr)";
 
-function hoverEffect() {
-      cells.addEventListener("mouseenter", () => console.log("Mouse enter"));
-      cells.style.color = "black";
-      cells.addEventListener("mouseleave", () => console.log("Mouse leave"));
-      cells.style.color = "white";
+  function makeGrids() {
+    for (let i = 0; i < 256; i++) {
+      const cells = document.createElement("cells");
+      grids.append(cells);
+
+      function hoverEffect() {
+        cells.addEventListener("mouseenter", () => console.log("Mouse enter"));
+        cells.style.backgroundColor = "black";
+        cells.addEventListener("mouseleave", () => console.log("Mouse leave"));
+        cells.style.backgroundColor = "white";
+      }
+      hoverEffect();
     }
-    hoverEffect();
   }
+
+  makeGrids();
 }
 
-makeGrids();
+createCells();
