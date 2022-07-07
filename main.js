73decfb393a536896126c592c70356b0bfd6df7d
memoryfile <cooperlewis@protonmail.com>
2022-07-06 20:13:06 -0400

    Attempt to change cursor color, & add comments

--- main.js
+++ main.js
@@ -2,18 +2,26 @@ const blackButton = document.getElementById("blackButton");
 const whiteButton = document.getElementById("whiteButton");
 const randomButton = document.getElementById("randomButton");
 const inputButton = document.getElementById("inputButton");
+// const inputButtonVar = prompt(
+//   "How many squares per side do you want in a grid?"
+// );
+
+// On/off switch for all other functions
 
 function createCells() {
   const grids = document.querySelector("#grids");
-  // grids.style.gridTemplateColumns = "repeat(16, 1fr)";
-  // grids.style.gridTemplateRows = "repeat(16, 1fr)";
+
+  // Make grid
 
   function makeGrids() {
     for (let i = 0; i < 256; i++) {
       const cells = document.createElement("cells");
       grids.append(cells);
 
+      // Allow a mouse to interact with the grid
+
       function hoverEffect() {
+        // if (cellsBecomeWhite()) {
         cells.addEventListener(
           "mouseenter",
           () => (cells.style.backgroundColor = "black")
@@ -22,8 +30,21 @@ function createCells() {
           "mouseleave",
           () => (cells.style.backgroundColor = "white")
         );
+        // }
+        // if (cellsBecomeBlack()) {
+        //   cells.addEventListener(
+        //     "mouseenter",
+        //     () => (cells.style.backgroundColor = "white")
+        //   );
+        //   cells.addEventListener(
+        //     "mouseleave",
+        //     () => (cells.style.backgroundColor = "black")
+        //   );
+        // }
       }
       hoverEffect();
+
+      // Allow buttons to change grid color
 
       function cellsBecomeBlack() {
         blackButton.addEventListener(
@@ -52,15 +73,14 @@ function createCells() {
       }
       cellsBecomeRandom();
 
-      function inputButtonPrompt() {
-        let inputButtonVar = prompt("How many squares per side do you want in a grid?")
-        inputButton.addEventListener("click", () =>
-          alert(inputButtonVar)
-        );
-        // if (inputButtonVar != null) {
-        //     return;
-        // }
-      }
+      // function inputButtonPrompt() {
+      //   document
+      //     .getElementById("inputButton")
+      //     .addEventListener("click", () => alert(inputButtonVar));
+      //   // if (inputButtonVar != null) {
+      //   //     return;
+      //   // }
+      // }
     }
   }
 
@@ -68,4 +88,3 @@ function createCells() {
 }
 
 createCells();
-
