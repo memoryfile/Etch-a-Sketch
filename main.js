a2ced2af1ad261ce10dbf855c6dfa0393361da17
memoryfile <cooperlewis@protonmail.com>
2022-07-15 18:17:43 -0400

    Pass `size` to makeBoard() via prompt

--- main.js
+++ main.js
@@ -5,111 +5,90 @@ const inputButton = document.getElementById("inputButton");
 const resetButton = document.getElementById("resetButton");
 let board = document.querySelector(".board");
 let content = document.querySelector(".content");
-let size = Number(
-  window.prompt("How many squares per side do you want in the grid?", 16)
-);
+let cellGrid = board.querySelectorAll("div");
+let cell = document.createElement("div");
+let size = 16;
 
-// On/off switch for all other functions
-
-function createCells() {
-  // Make board
-
-  function makeBoard() {
-    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-    for (let i = 0; i < size * size; i++) {
-      let cells = document.createElement("cells");
-      cells.style.backgroundColor = "grey";
-
-      // Another way to 'append'
-
-      board.insertAdjacentElement("beforeend", cells);
-
-
-      // inputButton.addEventListener("click", promptSize => {
-      //   if (size === null || size === NaN || size < 2 || size > 100) {
-      //     return console.log("Could not input grid size, not a valid number.");
-      //   } else {
-      //     console.log(size);
-      //     makeBoard(size);
-      //   }
-      // });
-      inputButton.addEventListener("click", promptSize);
-      function promptSize() {
-        Number(
-          window.prompt("How many squares per side do you want in the grid?", 16)
-        );
-      }
-
-      // Allow mouse clicks to interact with the grid
-
-      function clickEffect() {
-        // if (cellsBecomeWhite()) {
-        cells.addEventListener(
-          "click",
-          () => (cells.style.backgroundColor = "black")
-        );
-        cells.addEventListener(
-          "click",
-          () => (cells.style.backgroundColor = "white")
-        );
-      }
-      clickEffect();
-
-      // Allow buttons to change grid
-
-      function cellsBecomeBlack() {
-        blackButton.addEventListener(
-          "click",
-          () => (cells.style.backgroundColor = "black")
-        );
-      }
-      cellsBecomeBlack();
-
-      function cellsBecomeWhite() {
-        whiteButton.addEventListener(
-          "click",
-          () => (cells.style.backgroundColor = "white")
-        );
-      }
-      cellsBecomeWhite();
-
-      function cellsReset() {
-        resetButton.addEventListener(
-          "click",
-          () => (cells.style.backgroundColor = "white")
-        );
-      }
-      cellsReset();
-
-      function cellsBecomeRandom() {
-        randomButton.addEventListener(
-          "click",
-          () =>
-            (cells.style.backgroundColor =
-              "#" +
-              (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6))
-        );
-      }
-      cellsBecomeRandom();
-    }
+function makeBoard(size) {
+  let amount = size * size;
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+  for (let i = 0; i < amount; i++) {
+    cell.style.backgroundColor = "grey";
+    board.insertAdjacentElement("beforeend", cell);
   }
+}
+makeBoard(size);
 
-  makeBoard();
+inputButton.addEventListener("click", promptSize);
+
+function promptSize() {
+  let size = Number(
+    window.prompt("How many squares per side do you want in the grid?", 16)
+  );
+  if (size === null || size === NaN || size < 2 || size > 100) {
+    return console.log("Could not input grid size, not a valid number.");
+  } else {
+    return makeBoard(size);
+  }
 }
 
-createCells();
+// Allow mouse clicks to interact with the grid
 
-// Cells coloring hover effect (can use instead of a click effect)
+function clickEffect() {
+  cell.addEventListener(
+    "click",
+    () => (cell.style.backgroundColor = "white")
+  );
+}
+clickEffect();
+
+// Allow buttons to change grid
+
+function cellBecomeBlack() {
+  blackButton.addEventListener(
+    "click",
+    () => (cell.style.backgroundColor = "black")
+  );
+}
+cellBecomeBlack();
+
+function cellBecomeWhite() {
+  whiteButton.addEventListener(
+    "click",
+    () => (cell.style.backgroundColor = "white")
+  );
+}
+cellBecomeWhite();
+
+function cellReset() {
+  resetButton.addEventListener(
+    "click",
+    () => (cell.style.backgroundColor = "white")
+  );
+}
+cellReset();
+
+function cellBecomeRandom() {
+  randomButton.addEventListener(
+    "click",
+    () =>
+      (cell.style.backgroundColor =
+        "#" + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6))
+  );
+}
+cellBecomeRandom();
+
+// cell coloring hover effect (can use instead of a click effect)
 
 // function hoverEffect() {
-//   cells.addEventListener(
+//   cell.addEventListener(
 //     "mouseenter",
-//     () => (cells.style.backgroundColor = "black")
+//     () => (cell.style.backgroundColor = "black")
 //   );
-//   cells.addEventListener(
+//   cell.addEventListener(
 //     "mouseleave",
-//     () => (cells.style.backgroundColor = "white")
+//     () => (cell.style.backgroundColor = "white")
 //   );
 // }
 // hoverEffect();
